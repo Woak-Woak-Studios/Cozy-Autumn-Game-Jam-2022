8fe8f733860ba78403a6f9c22b2bb07a96eaa419
SerialBoxes <roxannecampbell244@gmail.com>
2022-09-09 15:10:00 -0700

    Prototype V1
    I made a test room, added panning, and started implementing held objects.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Camera cam;
+
+    Vector2 cursorPos;
+    Vector2 cursorPosLast;
+    Vector2 cursorPosRead;
+    bool panning;
+
+    LayerMask maskPlaceSurfaces;
+    LayerMask maskHoldableObjects;
+    bool holding;
+    Transform heldObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cursorPos = Vector2.zero;
+        cursorPosLast = Vector2.zero;
+
+        maskPlaceSurfaces = LayerMask.GetMask("Place Surface");
+        maskHoldableObjects = LayerMask.GetMask("Holdable Objects");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cursorPosLast = cursorPos;
+        cursorPos = cursorPosRead;
+        Panning();
+
+        Holding();
+    }
+
+//INPUT CALLBACKS
+    public void CursorPositionChanged(InputAction.CallbackContext callback){
+        cursorPosRead = callback.ReadValue<Vector2>();
+    }
+
+    public void OnPanClick(InputAction.CallbackContext callback){
+        panning = callback.ReadValueAsButton();
+    }
+
+    public void OnSelectClick(InputAction.CallbackContext callback){
+        bool val = callback.ReadValueAsButton();
+        if (val && callback.started){
+            Select();
+        }
+    }
+
+//SELECTION CODE
+    void Select(){
+        if (!holding){
+            Ray ray = cam.ScreenPointToRay(cursorPos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100,maskHoldableObjects)){
+                holding = true;
+                heldObject = hit.transform;
+            }
+        }
+    }
+
+    void Holding(){
+        if ( holding && heldObject != null){
+            heldObject.position = GetMouseWorldPosition(cursorPos,5f);
+        }
+    }
+
+//PANNING CODE
+    void Panning(){
+        if (panning){
+            MoveScreen();
+        }
+    }
+
+    void MoveScreen(){
+        MoveScreen(5f);
+    }
+
+    void MoveScreen (float dist){
+        Vector3 oldPos = GetMouseWorldPosition(cursorPosLast,dist);
+        Vector3 newPos = GetMouseWorldPosition(cursorPos,dist);
+        Vector3 delta = oldPos-newPos;
+        transform.position += transform.right * Vector3.Dot(transform.right,delta);
+        transform.position += transform.up * Vector3.Dot(transform.up,delta);
+    }
+
+    Vector3 GetMouseWorldPosition(Vector2 position, float dist){
+        Vector3 pos3D = new Vector3(position.x,position.y,dist);
+        return cam.ScreenToWorldPoint(pos3D);
+    }
+}
